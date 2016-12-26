3303ce174a459c3228a05ff98b8b1bb8119470a0
zhaojianhui <zhaojianhui129@163.com>
2016-12-26 12:01:02 +0800

    更新上传组件,七牛直接上传

--- app/library/MyTag.php
+++ app/library/MyTag.php
@@ -46,41 +46,42 @@ class MyTag extends Tag
                 $defaultValue && $parameters['initialContent'] = htmlspecialchars($defaultValue);
         }
 
-        $code .= "var ue_" . $id . " = UE.getEditor('" . $id . "', " . PHP_EOL . json_encode($parameters, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK) . ");" . PHP_EOL;
+        $editorName = 'ue_'.$id;
+        $code .= "var {$editorName} = UE.getEditor('" . $id . "', " . PHP_EOL . json_encode($parameters, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK) . ");" . PHP_EOL;
 
         switch ($type) {
             case 'files':
-                $code .= 'ue_'.$id.'.ready(function () {
+                $code .= "{$editorName}.ready(function () {
                         //侦听文件上传
-                        ue_'.$id.'.addListener(\'afterUpfile\', function (t, arg) {
-                            if(typeof('.$id.'Callback) === "function"){
-                                '.$id.'Callback(arg);
+                        {$editorName}.addListener('afterUpfile', function (t, arg) {
+                            if(typeof({$id}Callback) === 'function'){
+                                {$id}Callback(arg);
                             }else{
-                                console.log("您未设置回调方法['.$id.'Callback(fileList)]");
+                                console.log('您未设置回调方法[{$id}Callback(fileList)]');
                             }
                         })
-                    });' . PHP_EOL;
-                $code .= '$("#'.$id.'_btn").click(function(){
-                        var '.$id.'Files = ue_'.$id.'.getDialog("attachment");
-                        '.$id.'Files.open();
-                    });';
+                    });" . PHP_EOL;
+                $code .= "$('#{$id}_btn').click(function(){
+                        var {$id}Files = {$editorName}.getDialog('attachment');
+                        {$id}Files.open();
+                    });";
                 break;
             case 'images':
-                $code .= 'ue_'.$id.'.ready(function () {
+                $code .= "{$editorName}.ready(function () {
                         //侦听图片上传
-                        ue_'.$id.'.addListener(\'beforeInsertImage\', function (t, arg) {
+                        {$editorName}.addListener('beforeInsertImage', function (t, arg) {
                             //调用回调方法
-                            if(typeof('.$id.'Callback) === "function"){
-                                '.$id.'Callback(arg);
+                            if(typeof({$id}Callback) === 'function'){
+                                {$id}Callback(arg);
                             }else{
-                                console.log("您未设置回调方法['.$id.'Callback(fileList)]");
+                                console.log('您未设置回调方法[{$id}Callback(fileList)]');
                             }
                         })
-                    });' . PHP_EOL;
-                $code .= '$("#'.$id.'_btn").click(function(){
-                        var '.$id.'Image = ue_'.$id.'.getDialog("insertimage");
-                        '.$id.'Image.open();
-                    });' . PHP_EOL;
+                    });" . PHP_EOL;
+                $code .= "$('#{$id}_btn').click(function(){
+                        var {$id}Image = {$editorName}.getDialog('insertimage');
+                        {$id}Image.open();
+                    });" . PHP_EOL;
                 break;
         }
 
@@ -97,11 +98,11 @@ class MyTag extends Tag
      *
      * @param array $config
      */
-    public static function webuploader($id, $fileType = '', $parameters = [])
+    public static function webuploader($id, $fileType = '', $uploadType = 'local', $parameters = [])
     {
         $di = self::getDI();
         $code = '';
-        $code .= '<script type="text/javascript">' . PHP_EOL;
+        $code .= "<script type=\"text/javascript\">" . PHP_EOL;
         //选择文件的按钮。可选。
         //内部根据当前运行是创建，可能是input元素，也可能是flash.
         $parameters['pick'] = isset($parameters['pick']) ? $parameters['pick'] : "#" . $id . '_btn';
@@ -115,7 +116,13 @@ class MyTag extends Tag
         //设置文件上传域的name
         //$parameters['fileVal'] = 'upfile';
         //文件接受服务器
-        $parameters['server'] = isset($parameters['server']) ? $parameters['server'] : $di['url']->get('upload/uedituploader', ['action' => 'uploadimage']);
+        switch ($uploadType){
+            case 'qiniu':
+                $parameters['server'] = 'http://up.qiniu.com/';
+                break;
+            default:
+                $parameters['server'] = isset($parameters['server']) ? $parameters['server'] : $di['url']->get('upload/uedituploader', ['action' => 'uploadimage']);
+        }
         switch ($fileType) {
 
             case 'images':
@@ -148,20 +155,21 @@ class MyTag extends Tag
                 thumbnailWidth = 200 * ratio,
                 thumbnailHeight = 200 * ratio,
                 uploader_" . $id . ";";
-        $code .= "var uploader_$id = WebUploader.create(" . json_encode($parameters, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK) . ");" . PHP_EOL;
+        $uploadName = 'uploader_'.$id;
+        $code .= "var {$uploadName} = WebUploader.create(" . json_encode($parameters, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK) . ");" . PHP_EOL;
         //当有文件添加进来的时候
         if ($fileType == 'images') {
-            $code .= "uploader_$id.on( 'uploadSuccess', function( file, response ) {
+            $code .= "{$uploadName}.on( 'uploadSuccess', function( file, response ) {
                 if(response.state == 'SUCCESS'){
-                    if(!$('#preview_$id').length){
+                    if(!$('#preview_{$id}').length){
                         $('" . $parameters['pick'] . "').after('<div id=\"preview_$id\" class=\"col-sm-9 thumbnail\" style=\"margin: 0px;\"><img></div>');
                     }
                     // 创建缩略图
-                    uploader_$id.makeThumb( file, function( error, src ) {
+                    uploader_{$id}.makeThumb( file, function( error, src ) {
                         if ( error ) {
                             return;
                         }
-                        $('#preview_$id img').attr( 'src', src );
+                        $('#preview_{$id} img').attr( 'src', src );
                     }, thumbnailWidth, thumbnailHeight );
                     if($('#{$id}_val').length){
                        $('#{$id}_val').val(response.url); 
@@ -170,39 +178,67 @@ class MyTag extends Tag
                     $.alert({title: '提示',content: '上传失败',autoClose: 'ok|1000',});
                 }
             });" . PHP_EOL;
-            $code .= "if($('#" . $id . "_val').val() != '' && $('#preview_$id').length){
-                $('" . $parameters['pick'] . "').after('<div id=\"preview_$id\" class=\"col-sm-9 thumbnail\" style=\"margin: 0px;\"><img></div>');
+            $code .= "if($('#{$id}_val').val() != '' && $('#preview_{$id}').length){
+                $('" . $parameters['pick'] . "').after('<div id=\"preview_{$id}\" class=\"col-sm-9 thumbnail\" style=\"margin: 0px;\"><img></div>');
             }";
         }
+
+        switch ($uploadType){
+            case 'qiniu':
+                $code .= "{$uploadName}.on('uploadBeforeSend', function (file, data, header) {
+                        //这里可以通过data对象添加POST参数
+                        header['X_Requested_With'] = 'XMLHttpRequest';
+                        $.ajax({
+                            dataType:'json',
+                            async:false,
+                            url:url,
+                            data:{
+                                type:'image',
+                                fileName:file.file.name,
+                                fileSize:file.file.size,
+                                ext:file.file.ext,
+                            },
+                            success:function(rs) {
+                                data.key = rs.key;
+                                data.token = rs.token;
+                            }
+                        });
+                    });";
+                break;
+            default:
+                $code .= "{$uploadName}.on('uploadBeforeSend', function( block, data, headers) {  
+                            data.key = new Date().toLocaleTimeString();  
+                        });";
+        }
         //文件上传过程中创建进度条实时显示。
-        $code .= "uploader_$id.on( 'uploadProgress', function( file, percentage ) {
-            if($('#" . $id . "_progress').length){
-                var percent = $('#" . $id . "_progress .progress span');
-        
-                // 避免重复创建
-                if ( !percent.length ) {
-                    $('#" . $id . "_progress').html('<p class=\"progress progress-xxs\"><span class=\"progress-bar progress-bar-success progress-bar-striped\"></span></p>');
-                }
-                percent.css( 'width', percentage * 100 + '%' );
-            }
-        });" . PHP_EOL;
+        $code .= "{$uploadName}.on( 'uploadProgress', function( file, percentage ) {
+                    if($('#{$id}_progress').length){
+                        var percent = $('#{$id}_progress .progress span');
+                
+                        // 避免重复创建
+                        if ( !percent.length ) {
+                            $('#" . $id . "_progress').html('<p class=\"progress progress-xxs\"><span class=\"progress-bar progress-bar-success progress-bar-striped\"></span></p>');
+                        }
+                        percent.css( 'width', percentage * 100 + '%' );
+                    }
+                });" . PHP_EOL;
         //文件上传成功，给item添加成功class, 用样式标记上传成功。
-        $code .= "uploader_$id.on( 'uploadSuccess', function( file ) {
-            $( '#'+file.id ).addClass('upload-state-done');
-        });" . PHP_EOL;
+        $code .= "{$uploadName}.on( 'uploadSuccess', function( file ) {
+                    $( '#'+file.id ).addClass('upload-state-done');
+                });" . PHP_EOL;
         //文件上传失败，显示上传出错。
-        $code .= "uploader_$id.on( 'uploadError', function( file ) {
-            $.alert({title: '提示',content: '上传失败',autoClose: 'ok|1000',});
-        });" . PHP_EOL;
+        $code .= "{$uploadName}.on( 'uploadError', function( file ) {
+                    $.alert({title: '提示',content: '上传失败',autoClose: 'ok|1000',});
+                });" . PHP_EOL;
         //完成上传完了，成功或者失败，先删除进度条。
-        $code .= "uploader_$id.on( 'uploadComplete', function( file ) {
-                $('#" . $id . "_progress').find('.progress').remove();
-            });" . PHP_EOL;
+        $code .= "{$uploadName}.on( 'uploadComplete', function( file ) {
+                    $('#{$id}_progress').find('.progress').remove();
+                });" . PHP_EOL;
         $code .= "$('" . $parameters['pick'] . "').mouseenter(function(){
-            uploader_$id.refresh();
-        });" . PHP_EOL;
+                    uploader_{$id}.refresh();
+                });" . PHP_EOL;
         $code .= "});" . PHP_EOL;
-        $code .= '</script>' . PHP_EOL;
+        $code .= "</script>" . PHP_EOL;
 
         return $code;
     }
