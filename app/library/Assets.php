063a30b54af6dce006bb42293f9de8df9db68bb8
zhaojianhui <zhaojianhui129@163.com>
2016-12-27 15:25:38 +0800

    项目静态资源全部移动到七牛上

--- app/library/Assets.php
+++ app/library/Assets.php
@@ -37,10 +37,12 @@ class Assets extends Component
 
         $isLocal = $this->config->environment == 'dev' ? true : false;
 
+        $version = 201612271308;
+
         //Bootstrap 3.3.7
         if (in_array('bootstrap', $this->assetsList)) {
-            $headerCss->addCss('//static.tudouyu.cn/bootstrap/bootstrap.min.css', false);
-            $footerJs->addJs('//static.tudouyu.cn/bootstrap/bootstrap.min.js', false);
+            $headerCss->addCss('//static.tudouyu.cn/bootstrap/3.3.7/css/bootstrap.min.css?v='.$version, false);
+            $footerJs->addJs('//static.tudouyu.cn/bootstrap/3.3.7/js/bootstrap.min.js?v='.$version, false);
         }
         //Font-Awesome
         if (in_array('Font-Awesome', $this->assetsList)) {
@@ -48,124 +50,68 @@ class Assets extends Component
         }
         ///Ionicons
         if (in_array('Ionicons', $this->assetsList)) {
-            $headerCss->addCss('//cdn.bootcss.com/ionicons/2.0.1/css/ionicons.min.css', false);
+            $headerCss->addCss('//static.tudouyu.cn/ionicons/2.0.1/css/ionicons.min.css', false);
         }
         //AdminLTE
         if (in_array('AdminLTE', $this->assetsList)) {
-            if ($isLocal) {
-                $headerCss->addCss('AdminLTE/dist/css/AdminLTE.min.css');
-                $headerCss->addCss('AdminLTE/dist/css/skins/_all-skins.min.css');
-                //AdminLTE App
-                $footerJs->addJs('AdminLTE/dist/js/app.min.js');
-                //AdminLTE for demo purposes
-                $footerJs->addJs('AdminLTE/dist/js/demo.js');
-            } else {
-                $headerCss->addCss('//cdn.bootcss.com/admin-lte/2.3.8/css/AdminLTE.min.css', false);
-                $headerCss->addCss('//cdn.bootcss.com/admin-lte/2.3.8/css/skins/_all-skins.min.css', false);
-                $footerJs->addJs('//cdn.bootcss.com/admin-lte/2.3.8/js/app.min.js', false);
-                $footerJs->addJs('//cdn.bootcss.com/admin-lte/2.3.8/js/demo.js', false);
-            }
+            $headerCss->addCss('//static.tudouyu.cn/AdminLTE/2.3.8/css/AdminLTE.min.css?v='.$version, false);
+            $headerCss->addCss('//static.tudouyu.cn/AdminLTE/2.3.8/css/skins/_all-skins.min.css', false);
+            $footerJs->addJs('//static.tudouyu.cn/AdminLTE/2.3.8/js/app.min.js', false);
+            $footerJs->addJs('//static.tudouyu.cn/AdminLTE/2.3.8/js/demo.js', false);
         }
         //jQuery2.2.4
         if (in_array('jQuery2.2.4', $this->assetsList)) {
-            if ($isLocal) {
-                $headerJs->addJs('plugins/jQuery/jquery-2.2.4.min.js');
-            } else {
-                $headerJs->addJs('//cdn.bootcss.com/jquery/2.2.4/jquery.min.js', false);
-            }
+            $headerJs->addJs('//static.tudouyu.cn/jQuery/jquery-2.2.4.min.js', false);
         }
         //jQuery1.12.4
         if (in_array('jQuery1.12.4', $this->assetsList)) {
-            if ($isLocal) {
-                $headerJs->addJs('plugins/jQuery/jquery-1.12.4.min.js');
-            } else {
-                $headerJs->addJs('//cdn.bootcss.com/jquery/1.12.4/jquery.min.js', false);
-            }
+            $headerJs->addJs('//static.tudouyu.cn/jQuery/jquery-1.12.4.min.js', false);
         }
         //jQuery1.10.2
         if (in_array('jQuery1.10.2', $this->assetsList)) {
-            if ($isLocal) {
-                $headerJs->addJs('plugins/jQuery/jquery-1.10.2.min.js');
-            } else {
-                $headerJs->addJs('//cdn.bootcss.com/jquery/1.10.2/jquery.min.js', false);
-            }
+            $headerJs->addJs('//static.tudouyu.cn/jQuery/jquery-1.10.2.min.js', false);
         }
         //jQuery-UI 1.11.4
         if (in_array('jQuery-UI', $this->assetsList)) {
-            if ($isLocal) {
-                $footerJs->addJs('plugins/jQueryUI/jquery-ui.min.js');
-            } else {
-                $footerJs->addJs('//cdn.bootcss.com/jqueryui/1.11.4/jquery-ui.min.js', false);
-            }
+            $footerJs->addJs('//static.tudouyu.cn/jQueryUI/1.11.4/jquery-ui.min.js', false);
         }
         //jquery-confirm files
         if (in_array('jquery-confirm', $this->assetsList)) {
-            if ($isLocal) {
-                $headerCss->addCss('plugins/jquery-confirm/jquery-confirm.min.css');
-                $headerJs->addJs('plugins/jquery-confirm/jquery-confirm.min.js');
-            } else {
-                $headerCss->addCss('//cdn.bootcss.com/jquery-confirm/3.0.1/jquery-confirm.min.css', false);
-                $headerJs->addJs('//cdn.bootcss.com/jquery-confirm/3.0.1/jquery-confirm.min.js', false);
-            }
+            $headerCss->addCss('//static.tudouyu.cn/jquery-confirm/3.0.1/jquery-confirm.min.css', false);
+            $headerJs->addJs('//static.tudouyu.cn/jquery-confirm/3.0.1/jquery-confirm.min.js', false);
         }
         //DataTables
         if (in_array('DataTables', $this->assetsList)) {
-            if ($isLocal) {
-                $headerCss->addCss('plugins/datatables/dataTables.bootstrap.css');
-                $headerJs->addJs('plugins/datatables/jquery.dataTables.min.js');
-                $headerJs->addJs('plugins/datatables/dataTables.bootstrap.min.js');
-            } else {
-                $headerCss->addCss('//cdn.bootcss.com/datatables/1.10.12/css/dataTables.bootstrap.min.css', false);
-                $headerJs->addJs('//cdn.bootcss.com/datatables/1.10.12/js/jquery.dataTables.min.js', false);
-                $headerJs->addJs('//cdn.bootcss.com/datatables/1.10.12/js/dataTables.bootstrap.min.js', false);
-            }
+            $headerCss->addCss('//static.tudouyu.cn/datatables/1.10.13/css/dataTables.bootstrap.min.css', false);
+            $headerJs->addJs('//static.tudouyu.cn/datatables/1.10.13/js/jquery.dataTables.min.js', false);
+            $headerJs->addJs('//static.tudouyu.cn/datatables/1.10.13/js/dataTables.bootstrap.min.js', false);
         }
         //Select2
         if (in_array('Select2', $this->assetsList)) {
-            if ($isLocal) {
-                $headerCss->addCss('plugins/select2/select2.min.css');
-                $headerJs->addJs('plugins/select2/select2.full.min.js');
-            } else {
-                $headerCss->addCss('//cdn.bootcss.com/select2/4.0.3/css/select2.min.css', false);
-                $headerJs->addJs('//cdn.bootcss.com/select2/4.0.3/js/select2.full.min.js', false);
-            }
+            $headerCss->addCss('//static.tudouyu.cn/select2/4.0.3/css/select2.min.css', false);
+            $headerJs->addJs('//static.tudouyu.cn/select2/4.0.3/js/select2.full.min.js', false);
         }
         //iCheck for checkboxes and radio inputs
         if (in_array('iCheck', $this->assetsList)) {
-            if ($isLocal) {
-                $headerCss->addCss('plugins/iCheck/all.css');
-                $headerJs->addJs('plugins/iCheck/icheck.min.js');
-            } else {
-                $headerCss->addCss('//cdn.bootcss.com/iCheck/1.0.2/skins/all.css', false);
-                $headerJs->addJs('//cdn.bootcss.com/iCheck/1.0.2/icheck.min.js', false);
-            }
+            $headerCss->addCss('//static.tudouyu.cn/iCheck/1.0.2/skins/all.css', false);
+            $headerJs->addJs('//static.tudouyu.cn/iCheck/1.0.2/icheck.min.js', false);
         }
         //bootstrapValidator
         if (in_array('bootstrapValidator', $this->assetsList)) {
-            if ($isLocal) {
-                $headerCss->addCss('plugins/bootstrapvalidator/bootstrapValidator.min.css');
-                $headerJs->addJs('plugins/bootstrapvalidator/bootstrapValidator.min.js');
-                $headerJs->addJs('plugins/bootstrapvalidator/language/zh_CN.js');
-            } else {
-                $headerCss->addCss('//cdn.bootcss.com/jquery.bootstrapvalidator/0.5.3/css/bootstrapValidator.min.css', false);
-                $headerJs->addJs('//cdn.bootcss.com/jquery.bootstrapvalidator/0.5.3/js/bootstrapValidator.min.js', false);
-                $headerJs->addJs('//cdn.bootcss.com/jquery.bootstrapvalidator/0.5.3/js/language/zh_CN.js', false);
-            }
+            $headerCss->addCss('//static.tudouyu.cn/bootstrapvalidator/0.5.3/css/bootstrapValidator.min.css', false);
+            $headerJs->addJs('//static.tudouyu.cn/bootstrapvalidator/0.5.3/js/bootstrapValidator.min.js', false);
+            $headerJs->addJs('//static.tudouyu.cn/bootstrapvalidator/0.5.3/js/language/zh_CN.js', false);
         }
         //UEditor,选用本地文件，修复点击上传图片时等待很慢的问题，
         //修复地址：https://github.com/fex-team/ueditor/issues/2983#issuecomment-239741212
         if (in_array('UEditor', $this->assetsList)){
-            $headerJs->addJs('plugins/ueditor/ueditor.config.js');
-            $headerJs->addJs('plugins/ueditor/ueditor.all.js');
-            //公式js
-//            $headerJs->addJs('plugins/ueditor/kityformula-plugin/addKityFormulaDialog.js');
-//            $headerJs->addJs('plugins/ueditor/kityformula-plugin/getKfContent.js');
-//            $headerJs->addJs('plugins/ueditor/kityformula-plugin/defaultFilterFix.js');
+            $headerJs->addJs('//static.tudouyu.cn/ueditor/1.4.3/ueditor.config.js', false);
+            $headerJs->addJs('//static.tudouyu.cn/ueditor/1.4.3/ueditor.all.min.js', false);
         }
         //webuploader
         if (in_array('webuploader', $this->assetsList)){
-            $headerCss->addCss('plugins/ueditor/third-party/webuploader/webuploader.css');
-            $headerJs->addJs('plugins/ueditor/third-party/webuploader/webuploader.min.js');
+            $headerCss->addCss('//static.tudouyu.cn/ueditor/1.4.3/third-party/webuploader/webuploader.css');
+            $headerJs->addJs('//static.tudouyu.cn/ueditor/1.4.3/third-party/webuploader/webuploader.min.js');
         }
     }
 
